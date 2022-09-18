002be1265bf0c60edf3cb6b97e1f681529394616
Cangyu Lin <jefflin.je598@gmail.com>
2022-09-19 01:19:29 +0800

    jython macro combination

---
+++ jython_macro.py
@@ -0,0 +1,61 @@
+import os, fnmatch, re, sys
+from ij import IJ, plugin
+
+tgt_path = "Images/"
+folders = []
+
+autoSort = False
+# autosort
+if autoSort:
+    for filename in os.listdir(tgt_path):
+        if os.path.splitext(filename)[1] == ".tiff":
+            if filename[0:6].upper() not in folders:
+                try:
+                    os.mkdir(filename[0:6].upper())
+                except:
+                    pass
+            os.rename(filename, filename[0:6].upper()+"/"+filename)
+    
+            folders.append(filename[0:6].upper())
+else:
+    for (dirpath, dirnames, filenames) in os.walk(tgt_path):
+        folders.extend(filenames)
+        break
+# find the maximums of channel, slice, frame
+ch = 0
+sl = 0
+fr = 0
+def find_max(folderName):
+    # Warning: before using the script, please make sure the filenames are in the correct format!!!
+
+    fileOfDirectory = folderName
+    # first pattern to filter all the tiffs with the prefix
+    patternPrefix = sys.argv[-1].lower()+"*"
+    folder = []
+
+    # append everything into the list
+    for filename in fileOfDirectory:
+        if fnmatch.fnmatch(filename, patternPrefix):
+                folder.append(filename)
+
+    global ch
+    global sl
+    global fr
+    
+    for filename in folder:
+        if(int(filename[15:16])>int(ch)):
+            ch = filename[15:16]
+        if(filename[18:20].isnumeric()):
+            if(int(filename[18:20])>int(sl)):
+                sl = filename[18:20]
+        else:
+            if(int(filename[18:19])>int(sl)):
+                sl = filename[18:19]
+
+        if(int(filename[10:11])>int(fr)):
+            fr = filename[10:11]
+
+for filename in folders
+    plugin.FolderOpener()
+    IJ.run()
+
