769b7135e5ae82109e2e358d3bf977ec5a8b799d
Cangyu Lin <jefflin.je598@gmail.com>
2022-12-24 22:12:14 +0800

    Add files via upload

--- jython_macro_gpu_threading.py
+++ jython_macro_gpu_threading.py
@@ -1,144 +1,74 @@
-import os, fnmatch, re, sys
-from ij import IJ, plugin
-from java.lang import Thread, InterruptedException
-from datetime import datetime
-# import gc
-
+import os
+from ij import IJ, macro
+from ij.plugin import FolderOpener, HyperStackConverter, ZProjector
 
 # @File(label='Destination directory', style="directory") save_path
 # @File(label='Source directory', style="directory") tgt_path
 tgt_path = IJ.getDirectory("Source directory")
 save_path = IJ.getDirectory("Destination directory")
 
-# debug the paths
-print(tgt_path)
-print(save_path)
-
-folders = []
-tries = 0
-# gc.enable()
-
-autoSort = False
-# autosort
-if autoSort:
-    for filename in os.listdir(tgt_path):
-        if os.path.splitext(filename)[1] == ".tiff":
-            if filename[0:6].upper() not in folders:
-                try:
-                    os.mkdir(os.path.join(tgt_path,filename[0:6].upper()))
-                except:
-                    pass
-            os.rename(os.path.join(tgt_path, filename), os.path.join(tgt_path, filename[0:6].upper(), filename))
-            folders.append(filename[0:6].upper())
-else:
-    for (dirpath, dirnames, filenames) in os.walk(tgt_path):
-        folders.extend(dirnames)
-        break
-print(folders)
-# find the maximums of channel, slice, frame
-ch = 0
-sl = 0
-fr = 0
-def find_max(folderName):
-    # Warning: before using the script, please make sure the filenames are in the correct format!!!
-
-    fileOfDirectory = os.listdir(os.path.join(tgt_path, folderName))
-    print("The path", os.path.join(tgt_path, folderName))
-    patternPrefix = folderName.lower()+"*"
-
-    folder = []
-
-    # append everything into the list
-    for filename in fileOfDirectory:
-        if fnmatch.fnmatch(filename, patternPrefix):
-            folder.append(filename)
-    print("files: ", folder)
-    global ch
-    global sl
-    global fr
+def process_folder(folder_name):
+    # Find the maximums
+    ch, sl, fr = 0, 0, 0
+    for file in os.listdir(os.path.join(tgt_path, folder_name)):
+        if file.startswith(folder_name.lower()):
+            ch = max(ch, int(file[15:16]))
+            if file[18:20].isnumeric():
+                fr = max(fr, int(file[18:20]))
+            else:
+                fr = max(fr, int(file[18]))
+            sl = max(sl, int(file[10:12]))
     
-    for filename in folder:
-        if(int(filename[15:16])>int(ch)):
-            ch = filename[15:16]
-        if(filename[18:20].isnumeric()):
-            if(int(filename[18:20])>int(fr)):
-                fr = filename[18:20]
-        else:
-            if(int(filename[18])>int(fr)):
-                fr = filename[18]
-        if(int(filename[10:12])>int(sl)):
-            sl = filename[10:12]
-
-    return (ch, sl, fr)
-
-# threading class
-class MacroThread(Thread):
-    # The constructor
-    def __init__(self, filename):
-        Thread.__init__(self)
-        # instance variables
-        self.filename = filename
-	
-    # The execution
-    def run(self):
-        # Find the maximums
-        # ch, sl, fr = find_max(filename)
-        ch,sl,fr=3,36,25
-        # print("channel: "+str(ch))
-        # print("slice: "+str(sl))
-        # print("frame: "+str(fr))
-        
-        # open the target folder
+    # Open the target folder
+    imp = FolderOpener.open(os.path.join(tgt_path,folder_name), "virtual")
+    
+    # Execute the macro of Z projection, and changing to hyperstack
+    # args_hyperstack = "order=xytcz channels={} slices={} frames={} display=Color".format(ch, sl, fr)
+    # IJ.run(imp, "Stack to Hyperstack...", args_hyperstack)
+    hsc = HyperStackConverter()
+    imp2 = hsc.toHyperStack(imp, ch, sl, fr, "xytcz", "color")
+    # hsc.shuffle(imp, hsc.TCZ)
+    
+    # zproj = ZProjector(imp2)
+    # zproj.setMethod(ZProjector.MAX_METHOD)
+    # zproj.doProjection()
+    # imp3 = zproj.getProjection()
+    # imp3.show()
+    if not os.path.exists(os.path.join(save_path, "hyperstack")):
         try:
-            if not os.path.exists(os.path.join(save_path, "Z-Proj", self.filename+"_MAX.tiff")) or not os.path.exists(os.path.join(save_path, "hyperstack", self.filename+"_hyperstack.tiff")):
-                self.imp = plugin.FolderOpener.open(os.path.join(tgt_path,self.filename), "virtual")
-        except:
-            print("something wrong happens!")
-        try:
-            global folders
-            global tries
-
-            args_hyperstack = "order=xytcz channels="+str(ch)+" slices="+str(sl)+" frames="+str(fr)+" display=Color"
-            # Execute the macro of Z projection, and changing to hyperstack
-            self.imp3 = IJ.run(self.imp, "Stack to Hyperstack...",args_hyperstack)
-            if not os.path.exists(os.path.join(save_path, "hyperstack")):
-                os.mkdir(os.path.join(save_path, "hyperstack"))
-            if not os.path.exists(os.path.join(save_path, "hyperstack", self.filename+"_hyperstack.tiff")):
-                IJ.saveAs(self.imp3, "Tiff", os.path.join(save_path,"hyperstack", self.filename+"_hyperstack.tiff"))
-            
-            # Z Projection
-            if not os.path.exists(os.path.join(save_path,"Z-Proj")):
-                os.mkdir(os.path.join(save_path, "Z-Proj"))
-            print("GPU doing z projection")
-            self.imp2 = IJ.run(self.imp3, "Maximum-Z-Projection frame by frame on multiple GPUs (experimental)", "")
-            print("GPU saving")
-            IJ.selectWindow("MaximumZProjectionFrameProcessor_"+self.filename)
-            if not os.path.exists(os.path.join(save_path, "Z-Proj", self.filename+"_MAX.tiff")):
-                IJ.saveAs(self.imp2, "Tiff", os.path.join(save_path, "Z-Proj", self.filename+"_MAX.tiff"))
-            
-            tries+=1
-            if(tries==len(folders)):        
-               # Close everything
-               IJ.run("Close All", "")
-           	   # Garbage Collection
-               IJ.run("Collect Garbage", "")
+            os.mkdir(os.path.join(save_path, "hyperstack"))
         except:
             pass
+    if not os.path.exists(os.path.join(save_path, "hyperstack", folder_name+"_hyperstack.tiff")):
+    	IJ.saveAs(imp2, "TIFF", os.path.join(save_path, "hyperstack", folder_name + "_hyperstack.tiff"))
+    IJ.run(imp2, "Maximum-Z-Projection frame by frame on multiple GPUs (experimental)", "")
+    # Save the maximum intensity projection if the image title includes "MAX"
+    # IJ.run(imp2, "Z Project...", "projection=[Max Intensity] all")
+    if not os.path.exists(os.path.join(save_path, "Z-Proj")):
+        try:
+            os.mkdir(os.path.join(save_path, "Z-Proj"))
+        except:
+            pass
+    IJ.selectWindow("MaximumZProjectionFrameProcessor_"+folder_name)
+    if not os.path.exists(os.path.join(save_path, "Z-Proj", folder_name+"_MAX.tiff")):
+        IJ.saveAs(  "TIFF", os.path.join(save_path, "Z-Proj", folder_name + "_MAX.tiff"))
+    print(imp2.getTitle())
+    imp.close()
+    imp2.close()
+    # imp3.close()
+    
+    
 
+# Run the macro on each folder in parallel using multithreading
+from threading import Thread
+threads = []
+for folder in os.listdir(tgt_path):
+    thread = Thread(target=process_folder, args=(folder,))
+    thread.start()
+    threads.append(thread)
 
+# Wait for all threads to finish
+for thread in threads:
+    thread.join()
 
-if __name__ == '__main__':
-    print("start time:",datetime.now())
-    times = 1
-    # run the macro folder by folder
-    for filename in folders:
-        times+=1
-        MacroThread(filename).start()
-        if(times%8 == 0):
-            MacroThread(filename).join()
-            
-        
-        
-            
-
-    print("end time:",datetime.now())
+IJ.showMessage("Processing complete.")
